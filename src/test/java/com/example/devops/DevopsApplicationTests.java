4d80bf3d6583b3e70eeef843b0483e066557488a
jinho9482 <157558300+jinho9482@users.noreply.github.com>
2024-05-21 12:14:07 +0900

    [Feat] Add h2 server to test

--- src/test/java/com/example/devops/DevopsApplicationTests.java
+++ src/test/java/com/example/devops/DevopsApplicationTests.java
@@ -1,13 +1,25 @@
 package com.example.devops;
 
+import com.example.devops.service.BoardService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class DevopsApplicationTests {
 
+	@Value("${spring.datasource.username}")
+	String username;
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void dbUserNameIsSa() {
+		assertEquals("sa", username);
 	}
 
 }
