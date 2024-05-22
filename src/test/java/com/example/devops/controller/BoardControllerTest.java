dbeebe683fe99a3b63c66bd0cb81a6b2c0991a7c
jinho9482 <157558300+jinho9482@users.noreply.github.com>
2024-05-22 18:00:18 +0900

    [Test] Add test code for controller

--- src/test/java/com/example/devops/controller/BoardControllerTest.java
+++ src/test/java/com/example/devops/controller/BoardControllerTest.java
@@ -1,10 +1,15 @@
 package com.example.devops.controller;
 
+import com.example.devops.dto.BoardRequest;
+import com.example.devops.entity.Board;
 import com.example.devops.global.BoardRepository;
 import com.example.devops.service.BoardService;
 import com.example.devops.service.BoardServiceImpl;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,37 +21,91 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(SpringExtension.class)
+
 @WebMvcTest(BoardController.class)
 class BoardControllerTest {
-    @Autowired
+    @Autowired // 통신을 위함
     private MockMvc mockMvc;
 
     @MockBean
     private BoardService boardService;
 
     @Test
-    void insertBoard() {
-
+    void insertBoard() throws Exception {
+        // given
+        BoardRequest boardRequest = new BoardRequest("Jinho", "Hola");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String body = objectMapper.writeValueAsString(boardRequest);
+        // when & then
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/boards")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(body))
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andDo(MockMvcResultHandlers.print());
     }
 
     @Test
     void getAll() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/boards/1"))
-                .andExpect(MockMvcResultMatchers.status().is5xxServerError());
+        // given
+        BDDMockito.given(boardService.getAll())
+                .willReturn(List.of(
+                        new Board(1L, "test1", "test1"),
+                        new Board(2L, "test2", "test2"),
+                        new Board(3L, "test3", "test3")
+                ));
+        // when & then
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/boards"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].id").value(1)) // 검증, $ : body를 의미
+                .andExpect(MockMvcResultMatchers.jsonPath("$.size()").value(3))
+                .andExpect(MockMvcResultMatchers.status().isOk()) // status code
+                .andDo(MockMvcResultHandlers.print()); // 뭔가를 하는 것
     }
 
-    @Test
-    void getOneById() {
-    }
+    @Nested
+    class getOneById {
+        @Test
+        void success() throws Exception {
+                // given
+                Long id = 1L;
+                BDDMockito.given(boardService.getOneById(id))
+                        .willReturn(new Board(1L, "Jinho", "Hola"));
+                // when & then
+                mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/boards/" + id))
+                        .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(1))
+                        .andExpect(MockMvcResultMatchers.status().isOk())
+                        .andDo(MockMvcResultHandlers.print());
+        }
 
+//        @Test
+//        void fail() throws Exception {
+//            // given
+//            Long id = 1L;
+//            BDDMockito.given(boardService.getOneById(id))
+//                    .willThrow(IllegalArgumentException.class);
+//            // when & then
+//            mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/boards/" + id))
+//                    .andExpect(MockMvcResultMatchers.status().isBadRequest())
+//                    .andDo(MockMvcResultHandlers.print());
+//        }
+
+    }
     @Test
-    void deleteById() {
+    void deleteById() throws Exception{
+        // given
+        Long id = 1L;
+        BDDMockito.doNothing().when(boardService).deleteById(id);
+        // when & then
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/v1/boards/" + id))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print());
     }
 }
--- src/test/java/com/example/devops/service/BoardServiceTest.java
+++ src/test/java/com/example/devops/service/BoardServiceTest.java
@@ -4,90 +4,56 @@ import com.example.devops.dto.BoardRequest;
 import com.example.devops.entity.Board;
 import com.example.devops.global.BoardRepository;
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest // spring을 실행시킨다. bean에 있는 것 (service, repository 등등)을 사용하고 싶을 때 사용
 class BoardServiceTest {
-    @Autowired
-    private BoardService boardService;
-    @Autowired
+    @InjectMocks
+    private BoardServiceImpl boardServiceImpl;
+    @Mock
     private BoardRepository boardRepository;
-
-    @Test
-    void insertUser() {
-        int lenBefore = boardRepository.findAll().size();
-        BoardRequest dto = new BoardRequest("Hi", "Hi");
-        boardService.insertBoard(dto);
-        List<Board> boards = boardRepository.findAll();
-        int lenAfter = boards.size();
-        Board lastOne = boards.get(lenAfter-1);
-
-        assertEquals(lenBefore+1, lenAfter);
-        assertEquals("Hi", lastOne.getName());
-        assertEquals("Hi", lastOne.getText());
-
-    }
-
-    @Test
-    void getAll() {
-        int lenBefore = boardService.getAll().size();
-        BoardRequest dto = new BoardRequest("Hi", "Hi");
-        boardRepository.save(dto.toEntity());
-
-        List<Board> boards = boardService.getAll();
-        int lenAfter = boards.size();
-        Board lastOne = boards.get(lenAfter-1);
-
-        assertEquals(lenBefore+1, lenAfter);
-        assertEquals("Hi", lastOne.getName());
-        assertEquals("Hi", lastOne.getText());
-
-    }
-
-    @Test
-    void getOneById() {
-        BoardRequest dto = new BoardRequest("Hola", "Hola");
-        boardRepository.save(dto.toEntity());
-
-        Long lenAfter = (long) boardRepository.findAll().size();
-        Board board = boardService.getOneById(lenAfter);
-
-        assertEquals("Hola", board.getName());
-        assertEquals("Hola", board.getText());
-    }
 
     @Test
     void deleteById() {
         // 매칭되는 id가 있을 때
         // given
-        Board board = new Board(null, "test", "test");
-        boardRepository.save(board); // entity manager 가 null 값을 채워서 다시 변환해준다.
-        Long id = board.getId();
-
+        Long id = 1L;
+        BDDMockito.doNothing().when(boardRepository).deleteById(id);
+        BDDMockito.given(boardRepository.findById(id))
+                .willReturn(Optional.of(new Board(1L, null, null)));
         // when
-        boardService.deleteById(id);
+        boardServiceImpl.deleteById(id);
 
         // then
-        assertEquals(0,boardRepository.findAll().size());
 
-//        BoardRequest dto = new BoardRequest("Hola", "Hola");
-//        boardRepository.save(dto.toEntity());
-//
-//        int len1 = boardRepository.findAll().size();
-//        boardService.deleteById((long) len1);
-//        int len2 = boardRepository.findAll().size();
-//
-//        assertEquals(len1-1, len2);
+
     }
 
     @Test
     void deleteById_fail() {
         // 매칭되는 id가 없을 때
-        assertThrows(IllegalArgumentException.class, () -> boardService.deleteById(1000L));
+
+        // given
+        Long id = 1000L;
+
+        BDDMockito.given(boardRepository.findById(id))
+                .willReturn(Optional.empty());
+        BDDMockito.doNothing().when(boardRepository).deleteById(id);
+        //when & then
+        assertThrows(IllegalArgumentException.class, () -> {
+            boardServiceImpl.deleteById(id);
+        });
+//        Mockito.verify(boardRepository, Mockito.times(1)).findById(id);
+
     }
 }
